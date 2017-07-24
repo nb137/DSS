859542688be2b8f0252d2c39debfc224b69f0fc3
nb137home <email>
2017-07-23 22:30:30 -0500

    Ch9 start 20170723

--- Ch8/Ch8.py
+++ Ch8/Ch8.py
@@ -55,9 +55,57 @@ def estimate_gradient(f, v, h=0.00001):
         for i, _ in enumerate(v)]
             
 def step(v, direction, step_size):
-    return [v_i + step_size + direction_i for v_i, direction_i in zip(v, direction)]
+    return [v_i + step_size * direction_i for v_i, direction_i in zip(v, direction)]
     
 def sum_of_squares_gradient(v):
     return [2 * v_i for v_i in v]
     
-v = [random.randint(-10,10) for i in range(3)]
+v = [random.randint(-10,10) for i in range(3)]  # rand starting point
+
+tolerance = 0.00001
+'''
+while True:
+    gradient = sum_of_squares_gradient(v)
+    next_v = step(v, gradient, -0.01)
+    if distance(next_v, v) < tolerance:
+        break
+    v = next_v
+'''
+    
+def safe(f):
+    def safe_f(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except:
+            return float('inf') # return inf if funct errors
+    return safe_f
+    
+step_sizes	=	[100,	10,	1,	0.1,	0.01,	0.001,	0.0001,	0.00001]
+
+def minimize_batch(target_fn, gradient_fn, theta_0, tolerance=0.000001):
+    global sstep_sizes
+    theta = theta_0
+    target_fn = safe(target_fn)    
+    value = target_fn(theta)
+    
+    while True:
+        gradient = gradient_fn(theta)
+        next_thetas = [step(theta,gradient, -step_size for step_size in step_sizes)]
+        if abs(value - next_value) < tolerance:
+            return theta
+        else:
+            theta, value = next_theta, next_value
+
+def negate(f):
+    return lambda *args, **kwargs: -f(*args,**kwargs)
+    
+def negate_all(f):
+    return lambda *args, **kwargs: [-y for y in f(*args, **kwargs)]
+
+def maximize_batch(target_fn, gradient_fn, theta_0, tolerance=0.000001):
+    return minimize_batch(negate(target_fn), negate_all(gradient_fn),
+                          theta_0, tolerance)
+
+#I bet scikitlearn has something for all this
+# stochastic gradient descent = pick one point at a time randomly and predict
+
---
+++ Ch9/Ch9.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 22 13:54:22 2017
+
+@author: nbrunner
+"""
+
+'''
+Check book repo for egrep.py which can grab stuff from cmd line
+'''
+
+# Using open, which I haven't done since LCO
+file_to_write = open('TestFile.txt','w')
+file_to_write.write("HELLO")
+file_to_write.close()
+
+
+import csv
+data=[]
+with open('tab_delimited_stock_prices.txt','rt') as f:
+    reader = csv.reader(f, delimiter='\t')
+    for row in reader:
+        date = row[0]
+        symbol = row[1]
+        closing_price = float(row[2])
+        data.append([date, symbol, closing_price])
+        
+data=[]
+with open('colon_delimited_stock_prices.txt', 'rt') as f:
+    reader = csv.DictReader(f, delimiter=':')
+    for row in reader:
+        date = row["date"]
+        symbol = row["symbol"]
+        closing_price = float(row["closing_price"])
+        data.append([date, symbol, closing_price])
+
+from bs4 import BeautifulSoup
+import requests
+site = "https://news.ycombinator.com/"
+html = requests.get(site).text
+soup = BeautifulSoup(html, 'lxml')
